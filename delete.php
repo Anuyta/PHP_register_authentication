2e698dac6f4eba411fc7f18f637ee46446816425
Anuyta <ryabtseva.anna.92@mail.ru>
2018-07-24 15:57:15 +0300

    add check to delete and update tasks

--- delete.php
+++ delete.php
@@ -7,8 +7,8 @@ if(empty($_POST['id']))
 }
 else {
     $db = ConnectionPDO::getInstance();
-    $stmt = $db->prepare('DELETE FROM tasks WHERE id_task = :id');
-    $stmt->execute([$_POST['id']]);
+    $stmt = $db->prepare('DELETE FROM tasks WHERE id_task = :id AND id_user = :idUser');
+    $stmt->execute(array('id' => [$_POST['id']], 'isUser' => $_SESSION['userId']));
 }
 
 
--- update.php
+++ update.php
@@ -1,10 +1,13 @@
 <?php
 require_once('ConnectionPDO.php');
+session_start();
 if(empty($_POST['updDate']) || empty($_POST['updTask']) || empty($_POST['id'])){
     echo 'updDate or updTask or id is null';
 }
 else{
     $db = ConnectionPDO::getInstance();
-    $stmt = $db->prepare('UPDATE tasks SET task_datetime = :newDate, task = :newTask WHERE id_task = :id');
-    $stmt->execute(array('newDate' => $_POST['updDate'], 'newTask' => $_POST['updTask'], 'id' => $_POST['id']));
+    $stmt = $db->prepare('UPDATE tasks SET task_datetime = :newDate, task = :newTask 
+                WHERE id_task = :id AND id_user = :idUser');
+    $stmt->execute(array('newDate' => $_POST['updDate'], 'newTask' => $_POST['updTask'], 'id' => $_POST['id'],
+        'id_user' => $_SESSION['userId']));
 }
